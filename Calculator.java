b622df7c79c312af9dcf306429efd3fa521dd73b
NADHINIS <115521562+NADHINIS@users.noreply.github.com>
2022-11-13 15:59:16 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class Calculator {
+public static void main(String[] args) {
+	int a=5;
+	int b=7;
+	int c=8;
+	//add three numbers
+	int sum=a+b+c;
+System.out.println("The sum is:"+ sum);
+int x = 12;
+int y= 2;
+//subtract two numbers
+int sub =x-y;
+System.out.println("The sub is:"+ sub);
+double m=123.56d;
+double n=213.76d;
+//multiply two numbers
+double mul =m*n;
+System.out.println("The mul is:"+mul);
+float s=400.5f;
+float r=20.5f;
+//divide these two numbers
+float divide=s/r;
+System.out.println("the divide is:"+divide);
+
+}
+
+}
+ 
