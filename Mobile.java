501b82d058098422207a62a901df443041e28027
NADHINIS <115521562+NADHINIS@users.noreply.github.com>
2022-11-13 16:07:06 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Mobile {
+	public void makecall() {
+		String mobileModel="Nokia";
+		float mobileWeight=12.23f;
+		System.out.println("Make a Call");
+		// TODO Auto-generated method stub
+
+	}
+	private void sendmsg() {
+		boolean isFullyCharged=false;
+		int mobileCost=30000;
+		System.out.println("Send Msg");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+public static void main(String[] args) {
+	Mobile obj=new Mobile();
+	obj.makecall();
+	obj.sendmsg();
+	System.out.println("This is my Mobile");
+	
+}
+
+	
+}
---
+++ Mymobile.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+
+
+public class Mymobile {
+	Mymobile makeCall=new Mymobile();
+	
+	public void Makecall () {
+		String mobilemodel="redmi";
+		float mobileWeight=12.35f;
+		
+			
+		
+		Mymobile sendMsg=new Mymobile();
+		boolean isFullyCharged=false;
+		int mobileCost=12000;
+		System.out.println(makeCall);
+		System.out.println(sendMsg);
+		System.out.println(mobileCost);
+		
+		// TODO Auto-generated method stub
+		}
+	}
+	
+		
+		
+
+
+
